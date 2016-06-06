bf43b731f964597a7c0bb98fb3911daa5a7018ec
Hyungmin Jun <hmjeon@MIT.EDU>
2016-06-05 22:18:40 -0400

    - updated cmd

--- AMG_Main_List.m
+++ AMG_Main_List.m
@@ -10,7 +10,7 @@ param.molmapResolution = 3;
 param.WindowSize       = [600 600];
 param.chimeraEXE       = '"C:\Program Files\Chimera 1.10.2\bin\chimera.exe"';
 param.chimeraOPTION    = '--silent --script';
-param.fileType         = 'all';     % cmd / tif / all
+param.fileType         = 'cmd';     % cmd / tif / all
 param.bulge            = 'no';      % yes / no
 
 %% Step 5. Read problem name from file
